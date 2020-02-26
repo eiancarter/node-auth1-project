5bdccb2577bd7837306345dee26f948539f1f303
Eian Carter <eian@bricleir.com>
2020-02-26 08:17:35 -0800

    mvp complete

--- api/server.js
+++ api/server.js
@@ -3,10 +3,9 @@ const apiRouter = require('./api-router');
 const configureMiddleware = require('./configure-middleware');
 const session = require("express-session")
 const server = express();
-
 configureMiddleware(server);
 
-server.use(session({
+const sessionConfig = {
     name: 'funsession',
     secret: 'please keep it secret',
     cookie: {
@@ -16,7 +15,16 @@ server.use(session({
     httpOnly: true, //prevents js use on cookie
     resave: false,
     saveUninitialized: false,
-}))
+    store: new KnexStore({
+        knex,
+        tablename: 'sessions',
+        createtable: true,
+        sidfieldname: 'sid',
+        clearInterval: 1000 * 60 * 15,
+    }),
+};
+
+server.use(session(sessionConfig));
 
 server.use(express.json());
 
