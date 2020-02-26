06ac666d601f9a32ebd9a4ed23e6a9a0c8644c78
Eian Carter <eian@bricleir.com>
2020-02-26 07:48:55 -0800

    added express server and session to server

--- api/server.js
+++ api/server.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const apiRouter = require('./api-router');
 const configureMiddleware = require('./configure-middleware');
+const session = require("express-session")
 const server = express();
 
 configureMiddleware(server);
+
+server.use(session({
+    name: 'funsession',
+    secret: 'please keep it secret',
+    cookie: {
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+        secure: false, // true in production, requires https
+    },
+    httpOnly: true, //prevents js use on cookie
+    resave: false,
+    saveUninitialized: false,
+}))
 
 server.use(express.json());
 
